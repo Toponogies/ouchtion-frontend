a294735745c6ae84fd9639c266807bbe7bf3e6de
Ha the hien <hathehien12a2@gmail.com>
2021-12-21 17:03:26 +0700

    add call auth module to backend

--- src/store/module/Auth/index.js
+++ src/store/module/Auth/index.js
@@ -2,7 +2,16 @@ import actions from "./actions";
 import mutations from "./mutations";
 
 export const AuthModule = {
-    state: {},
+    state: {
+        accessToken: null,
+        refreshToken: null,
+        loggingIn: false,
+        loginError: null,
+        refreshError: null,
+        registerError: null,
+        logoutError: null,
+        resetError: null,
+    },
     actions: actions,
     mutations: mutations,
 };
--- src/store/module/Auth/mutations.js
+++ src/store/module/Auth/mutations.js
@@ -1,1 +1,26 @@
-export default {};
+export default {
+    loginStart: (state) => (state.loggingIn = true),
+    logout: (state) => (state.loggingIn = false),
+    loginStop: (state, errorMessage) => {
+        state.loggingIn = false;
+        state.loginError = errorMessage;
+    },
+    updateAccessToken: (state, accessToken) => {
+        state.accessToken = accessToken;
+    },
+    updateRefreshToken: (state, refreshToken) => {
+        state.refreshToken = refreshToken;
+    },
+    refreshStop: (state, errorMessage) => {
+        state.refreshError = errorMessage;
+    },
+    registerStop: (state, errorMessage) => {
+        state.registerError = errorMessage;
+    },
+    logoutStop: (state, errorMessage) => {
+        state.logoutError = errorMessage;
+    },
+    resetStop: (state, errorMessage) => {
+        state.resetError = errorMessage;
+    },
+};
