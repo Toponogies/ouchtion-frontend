bb6f871064207456dbc510f79e791174263b4d08
Bùi Văn Thiện <thien.bui.84436@gmail.com>
2021-12-31 11:17:46 +0700

    Enhancements 1 (#10)
    * Add Prettier config.
    
    * Add Inter font, DayJS; update utils
    
    * Add demo home UI

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
     <v-app>
-        <router-view />
+        <v-container>
+            <v-row justify="center">
+                <v-col class="ma-8" lg="10" xl="8">
+                    <!-- Actual view starts here -->
+                    <top-bar></top-bar>
+                    <v-row id="content">
+                        <router-view :key="$router.path" />
+                    </v-row>
+                </v-col>
+            </v-row>
+        </v-container>
     </v-app>
 </template>
 
 <script>
+import "@fontsource/inter/variable.css";
+
+import TopBar from "./components/topbar/TopBar.vue";
+
 export default {
     name: "App",
+    components: { TopBar },
 };
 </script>
---
+++ src/utils/priceUtils.js
@@ -0,0 +1,3 @@
+// Group digits in price by the thousand
+// https://stackoverflow.com/a/3753507
+export const formatPrice = (price) => price.toLocaleString("en");
