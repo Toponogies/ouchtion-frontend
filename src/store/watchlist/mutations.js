e585a4dcc7e83deef1d5c603647ee1ac53e1fa51
Thien Bui <thien.bui.84436@gmail.com>
2022-01-16 23:34:59 +0700

    implement bidder watchlist

--- src/store/watchlist/mutations.js
+++ src/store/watchlist/mutations.js
@@ -1,1 +1,11 @@
-export default {};
+export default {
+    setLoadingState(state, isLoading) {
+        state.isLoading = isLoading;
+    },
+    setItems(state, items) {
+        state.watchlistItems = items;
+    },
+    removeItem(state, id) {
+        state.watchlistItems = state.watchlistItems.filter((each) => each.id !== id);
+    },
+};
