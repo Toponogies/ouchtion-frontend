3cb5606f4239fed8ea0005e06118982021c07ad4
Ha the hien <hathehien12a2@gmail.com>
2022-01-18 21:36:40 +0700

    call api watchlist, product won, ongoing

--- src/store/watchlist/actions.js
+++ src/store/watchlist/actions.js
@@ -1,26 +1,47 @@
 import { showSnack } from "@/utils/showSnack";
+import axios from "axios";
 
 export default {
-    fetchAll({ commit }) {
+    async fetchAll({ commit,rootState }) {
         commit("setLoadingState", true);
 
-        const data = [
-            {
-                primaryImage: "https://picsum.photos/200",
-                id: "1",
-                name: "Product name",
-                endTime: "2022-01-04T12:34:56.789Z",
-                highestBidPrice: 1699000,
-            },
-        ];
+        const data = [];
+
+        let products = await axios.get("http://localhost:3000/api/users/watchlist", {headers: {
+            'Authorization': 'Bearer '+ rootState.AuthModule.accessToken,
+        }}).then((response) => {
+                return response.data;
+        })
+        products.forEach(async product => {
+            product = await axios.get(`http://localhost:3000/api/products/${product.product_id}`).then((response) => {
+                return response.data;
+            })
+            data.push({
+                primaryImage: "http://localhost:3000/"+ product.avatar,
+                id: product.product_id,
+                name: product.name,
+                endTime: product.end_at,
+                highestBidPrice: product.current_price,
+            })
+        });
+
         setTimeout(() => {
             commit("setItems", data);
             commit("setLoadingState", false);
         }, 500);
     },
 
-    removeItem({ commit }, id) {
+    async removeItem({ commit,rootState }, id) {
         commit("setLoadingState", true);
+
+        await axios.delete("http://localhost:3000/api/users/watchlist", {
+            headers: {
+            'Authorization': 'Bearer '+ rootState.AuthModule.accessToken,
+            },
+            data: {
+                product_id: id,
+            },
+        })
 
         setTimeout(() => {
             commit("removeItem", id);
