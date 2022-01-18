04c8858313c67f5f1e1a53518bf0adeab0808bab
HaTheHien <47638822+HaTheHien@users.noreply.github.com>
2022-01-18 22:34:06 +0700

    call api get bidder dashboard (#28)
    * call api watchlist, product won, ongoing
    
    * add dispatch dorefresh when accessToken expired
    
    * fix missing dispatch in watchlist

--- src/store/bids/actions.js
+++ src/store/bids/actions.js
@@ -1,63 +1,121 @@
 import { showSnack } from "@/utils/showSnack";
+import axios from "axios";
 
 export default {
-    fetchOngoing({ commit }) {
+    async fetchOngoing({ commit,rootState,dispatch }) {
         commit("setOngoingBidsLoadingState", true);
-        const data = [
-            {
-                primaryImage: "https://picsum.photos/200",
-                id: "1",
-                name: "Product name",
-                endTime: "2022-01-04T12:34:56.789Z",
-                highestBidPrice: 1699000,
-            },
-        ];
+        const data = [];
+
+        let products = await axios.get("http://localhost:3000/api/products/bidding", {headers: {
+                'Authorization': 'Bearer '+ rootState.AuthModule.accessToken,
+            }}).then((response) => {
+                    return response.data;
+            }).catch( async (error) => {
+                console.log(error.response.data);
+                if (error.response.data && error.response.data.title === "EXPIRED_ACCESSTOKEN")
+                {
+                    await dispatch('AuthModule/doRefresh', null, { root: true });
+                }
+                return await axios.get("http://localhost:3000/api/products/bidding",{headers: {
+                    'Authorization': 'Bearer '+ rootState.AuthModule.accessToken,
+                }}).then((response) => {
+                    return response.data;
+                })
+                .catch((error) => {
+                   console.log(error.response.data)
+                   return [];
+                });
+            });
+        products.forEach(product => {
+            data.push({
+                primaryImage: "http://localhost:3000/"+ product.avatar,
+                id: product.product_id,
+                name: product.name,
+                endTime: product.end_at,
+                highestBidPrice: product.current_price,
+            })
+        });
         setTimeout(() => {
             commit("setOngoingBidsItems", data);
             commit("setOngoingBidsLoadingState", false);
         }, 500);
     },
 
-    fetchCompleted({ commit }) {
+    async fetchCompleted({ commit,rootState,dispatch }) {
         commit("setCompletedBidsLoadingState", true);
-        const data = [
-            {
-                primaryImage: "https://picsum.photos/200?random=1",
-                id: "1",
-                name: "Product name",
-                highestBidPrice: 1699000,
+        const data = [];
+
+        let products = await axios.get("http://localhost:3000/api/products/won", {headers: {
+                'Authorization': 'Bearer '+ rootState.AuthModule.accessToken,
+            }}).then((response) => {
+                    return response.data;
+            }).catch( async (error) => {
+                console.log(error.response.data);
+                if (error.response.data && error.response.data.title === "EXPIRED_ACCESSTOKEN")
+                {
+                    await dispatch('AuthModule/doRefresh', null, { root: true });
+                }
+                return await axios.get("http://localhost:3000/api/products/won",{headers: {
+                    'Authorization': 'Bearer '+ rootState.AuthModule.accessToken,
+                }}).then((response) => {
+                    return response.data;
+                })
+                .catch((error) => {
+                   console.log(error.response.data)
+                   return [];
+                });
+            });
+
+        let rates = await axios.get(`http://localhost:3000/api/users/rate`, {headers: {
+                'Authorization': 'Bearer '+ rootState.AuthModule.accessToken,
+        }}).then((response) => {
+            return response.data;
+        })
+
+        products.forEach(async product => {
+            let reviewToBidder = {
+                rating: null,
+                comment: null,
+            }
+
+            let reviewToSeller = {
+                rating: null,
+                comment: null,
+            }
+
+            rates.forEach(rate => {
+                if (rate.type === "SELLER-BUYER" && rate.product_id === product.product_id)
+                {
+                    reviewToBidder = {
+                        rating: rate.rate,
+                        comment: rate.comment,
+                    }
+                }
+                if (rate.type === "BUYER-SELLER" && rate.product_id === product.product_id)
+                {
+                    reviewToSeller = {
+                        rating: rate.rate,
+                        comment: rate.comment,
+                    }
+                }
+            });
+            data.push({
+                primaryImage: "http://localhost:3000/"+ product.avatar,
+                id: product.product_id,
+                name: product.name,
+                highestBidPrice: product.current_price,
 
                 // seller -> bidder, read-only
-                reviewToBidder: {
-                    rating: null,
-                    comment: null,
-                },
+                reviewToBidder: reviewToBidder,
 
                 // bidder -> seller, can only write once
-                reviewToSeller: {
-                    rating: null,
-                    comment: null,
-                },
-            },
-            {
-                primaryImage: "https://picsum.photos/200?random=2",
-                id: "2",
-                name: "Product name",
-                highestBidPrice: 1699000,
+                reviewToSeller: reviewToSeller,
+            })
+        });
 
-                // seller -> bidder, read-only
-                reviewToBidder: {
-                    rating: null,
-                    comment: null,
-                },
+        console.log(data);
 
-                // bidder -> seller, can only write once
-                reviewToSeller: {
-                    rating: null,
-                    comment: null,
-                },
-            },
-        ];
+        
         setTimeout(() => {
             commit("setCompletedBidsItems", data);
             commit("setCompletedBidsLoadingState", false);
