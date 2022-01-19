6b632d74909c87f7aa9856fb7235f95ab25919e9
Thien Bui <thien.bui.84436@gmail.com>
2022-01-19 22:24:43 +0700

    categories: fix wrong mutation name

--- src/store/categories/actions.js
+++ src/store/categories/actions.js
@@ -7,7 +7,7 @@ export default {
             const categories = await axios.get("http://localhost:3000/api/categorys").then((response) => {
                 return response.data;
             });
-            commit("updateCategories", categories);
+            commit("setCategories", categories);
         } catch (error) {
             console.log(error.response.data);
         }
