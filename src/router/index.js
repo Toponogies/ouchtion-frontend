23acb42f14b9246aad196c26420c5c51e2a2bbde
Thien Bui <thien.bui.84436@gmail.com>
2022-01-19 22:24:25 +0700

    router: prevent direct access on some routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+
 import Home from "@/views/Home";
 import SearchResult from "@/views/SearchResult";
 import ProductDetails from "@/views/ProductDetails";
@@ -7,9 +8,7 @@ import AdminDashboard from "@/views/AdminDashboard";
 import BidderDashboard from "@/views/BidderDashboard";
 import SellerDashboard from "@/views/SellerDashboard";
 import UserProfile from "@/views/UserProfile";
-import VerifyAccount from "@/views/VerifyAccount";
 import ProductCreator from "@/views/ProductCreator";
-import ResetPass from "@/views/ResetPass"
 
 Vue.use(VueRouter);
 
@@ -33,37 +32,44 @@ const routes = [
         path: "/dashboard/a",
         name: "AdminDashboard",
         component: AdminDashboard,
+        meta: {
+            preventDirectAccess: true,
+        },
     },
     {
         path: "/dashboard/b",
         name: "BidderDashboard",
         component: BidderDashboard,
+        meta: {
+            preventDirectAccess: true,
+        },
     },
     {
         path: "/dashboard/s",
         name: "SellerDashboard",
         component: SellerDashboard,
+        meta: {
+            preventDirectAccess: true,
+        },
     },
     {
         path: "/dashboard/s/new",
         name: "ProductCreator",
         component: ProductCreator,
+        meta: {
+            preventDirectAccess: true,
+        },
     },
     {
         path: "/profile",
         name: "UserProfile",
         component: UserProfile,
+        meta: {
+            preventDirectAccess: true,
+        },
     },
-    {
-        path: "/verify",
-        name: "VerifyAccount",
-        component: VerifyAccount,
-    },
-    {
-        path: "/reset",
-        name: "ResetPassAccount",
-        component: ResetPass,
-    },
+
+    // redirect on invalid route
     {
         path: "*",
         redirect: "/",
@@ -74,6 +80,19 @@ const router = new VueRouter({
     mode: "history",
     base: process.env.BASE_URL,
     routes,
+});
+
+// Global navigation guard
+// https://stackoverflow.com/a/52663166
+router.beforeEach((to, _, next) => {
+    // Prevent direct access to these route (can only access from UI)
+    // Dirty workaround (since we cannot be sure that the store is initialized before the route is navigated to.)
+
+    if (to.meta.preventDirectAccess) {
+        next({ name: "Home" });
+    } else {
+        next();
+    }
 });
 
 export default router;
