1757bfc9f0ec00fa88eddb71007de15bb1f92a2a
HaTheHien <47638822+HaTheHien@users.noreply.github.com>
2022-01-21 12:33:12 +0700

    add create product (#41)

---
+++ src/store/addProduct/index.js
@@ -0,0 +1,15 @@
+import getters from "./getters";
+import actions from "./actions";
+import mutations from "./mutations";
+
+export const AddProductModule = {
+    namespaced: true,
+
+    state: {
+        
+    },
+
+    getters,
+    actions,
+    mutations,
+};
