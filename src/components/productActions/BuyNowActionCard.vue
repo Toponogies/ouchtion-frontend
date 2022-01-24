aae1c733a6d325175fc5671257dd7bac0e2ce62e
Ha the hien <hathehien12a2@gmail.com>
2022-01-24 21:27:04 +0700

    fix socket rate, won, remove buy now socket
    Co-Authored-By: Bùi Văn Thiện <41188108+84436@users.noreply.github.com>
    Co-Authored-By: Phụng Bùi <alex.bui.vhp@gmail.com>

--- src/components/productActions/BuyNowActionCard.vue
+++ src/components/productActions/BuyNowActionCard.vue
@@ -1,10 +1,10 @@
 <template>
-    <v-card @click="handleConfirmDialogOpen" :color="cardColor" elevation="4" :disabled="!isBuyNowOptionAvailable && btnDisable">
+    <v-card @click="handleConfirmDialogOpen" :color="cardColor" elevation="4" :disabled="btnDisable || isBuyNowOptionAvailable == 0">
         <!-- Header + Decor -->
         <v-row no-gutters class="px-4 pt-4 pb-2">
             <div>BUY NOW</div>
             <v-spacer></v-spacer>
-            <v-icon dark v-if="isBuyNowOptionAvailable">mdi-arrow-right</v-icon>
+            <v-icon dark v-if="btnDisable || isBuyNowOptionAvailable == 0">mdi-arrow-right</v-icon>
         </v-row>
 
         <!-- Price -->
@@ -60,8 +60,8 @@ export default {
     },
 
     computed: {
+        ...mapState("CurrentProductModule", ["endTime", "bid", "isBlockedFromBidding", "isSold", "request","buyNow"]),
         ...mapState("CurrentUserModule", ["role"]),
-        ...mapState("CurrentProductModule", ["buyNow"]),
         isBuyNowOptionAvailable: function () {
             return this.buyNow.price !== null;
         },
@@ -72,8 +72,17 @@ export default {
             return this.isBuyNowOptionAvailable ? this.utils.formatPrice(this.buyNow.price) : "Not applicable";
         },
         btnDisable: function () {
-            if (this.role === ROLES.SELLER || this.role === ROLES.ADMIN)
+            if (this.role === ROLES.SELLER || this.role === ROLES.ADMIN) return true;
+
+            if (this.isSold) {
                 return true;
+            } else if (this.isBlockedFromBidding && !this.request.isSent && !this.request.isBlockedFromRequesting) {
+                return false;
+            } else if (this.isBlockedFromBidding && this.request.isSent && !this.request.isBlockedFromRequesting) {
+                return true;
+            } else if (this.isBlockedFromBidding && this.request.isSent && this.request.isBlockedFromRequesting) {
+                return true;
+            }
             return false;
         },
     },
@@ -92,7 +101,16 @@ export default {
 
                 // For bidders: show the modal
                 case ROLES.BIDDER:
-                    this.setBuyNowModalState(true);
+                    if (!this.isBlockedFromBidding) {
+                        this.newPrice = this.suggestedBidPrice;
+                        this.setBidModalState(true);
+                    } else if (
+                        this.isBlockedFromBidding &&
+                        !this.request.isSent &&
+                        !this.request.isBlockedFromRequesting
+                    ) {
+                        this.setBidRequestModalState(true);
+                    }
                     break;
 
                 // For everyone else: do nothing
